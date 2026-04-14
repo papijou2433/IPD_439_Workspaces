bdacf2b29994e7668bacc369eb43c359cf3a9d97
papijou2433 <sebastian.sanchezp@usm.cl>
2026-04-14 10:51:42 -0400

    unfisnished DAC testing & ppt update

---
+++ Proyecto/DAC_test/Core/Inc/callbacks.h
@@ -0,0 +1,13 @@
+/*
+ * callbacks.h
+ *
+ *  Created on: Apr 9, 2026
+ *      Author: sebas
+ */
+
+#ifndef INC_CALLBACKS_H_
+#define INC_CALLBACKS_H_
+
+
+#include "main.h"
+#endif /* INC_CALLBACKS_H_ */
